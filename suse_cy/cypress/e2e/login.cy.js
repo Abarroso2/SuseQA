9312debaeefcbd822e583b6bce785f4ca577d35c
Allan da Silva Barroso <50781919+Abarroso2@users.noreply.github.com>
2024-10-12 12:53:06 -0300

    Update login.cy.js
    credentials revoval

--- suse_cy/cypress/e2e/login.cy.js
+++ suse_cy/cypress/e2e/login.cy.js
@@ -5,8 +5,8 @@ context('login ranch', () => {
     cy.visit('https://localhost/dashboard/auth/login')
   })
   it('execute login action', () => {
-    cy.get('input#username').type('admin')
-    cy.get('.password').type('OTNeSWuQKlfCyhJ3')
+    cy.get('input#username').type('add user')
+    cy.get('.password').type('add pass')
     cy.get('button#submit').click()
     cy.get('.simple-title').should('be.visible')
     cy.get('h1').should('contain', 'Welcome to Rancher')
